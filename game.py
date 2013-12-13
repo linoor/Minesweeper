a8f83b14df6bcb4e9a005c2b38e82df883c8f934
Linoor <orange7up@gmail.com>
2013-12-13 10:30:55 +0100

    po przegranej nie mozna kliknac na pola

--- game.py
+++ game.py
@@ -4,37 +4,40 @@ import globals
 class Game:
 	def __init__(self, minefield):
 		self.minefield = minefield
+		self.clickable = True
 	def find_collide_rect(self, pos):
 		for b in self.minefield.blocks:
 			if b.rect.collidepoint(pos):
 				return b
 	def left_click(self, pos):
-		b = self.find_collide_rect(pos)
-		if b:
-			if b.covered and not b.flagged and not b.question:
-				b.uncover()	
-			print(b.rect)
-			print(pos)
+		if self.clickable:
+			b = self.find_collide_rect(pos)
+			if b:
+				if b.covered and not b.flagged and not b.question:
+					b.uncover()	
+				print(b.rect)
+				print(pos)
 
-		if self.is_game_over():
-			self.end_game()
-		self.minefield.update()
+			if self.is_game_over():
+				self.end_game()
+			self.minefield.update()
 	def right_click(self, pos):
-		b = self.find_collide_rect(pos)
-		if b:
-			if b.covered:
-				if not b.flagged and not b.question:
-					b.flag()
-				elif b.flagged:
-					Block.question(b)
-				elif b.question:
-					b.cover()
-			print(b.rect)
-			print(pos)
+		if clickable:
+			b = self.find_collide_rect(pos)
+			if b:
+				if b.covered:
+					if not b.flagged and not b.question:
+						b.flag()
+					elif b.flagged:
+						Block.question(b)
+					elif b.question:
+						b.cover()
+				print(b.rect)
+				print(pos)
 
-		if self.is_game_over():
-			self.end_game()
-		self.minefield.update()
+			if self.is_game_over():
+				self.end_game()
+			self.minefield.update()
 	def is_game_over(self):
 		# jesli trafimy na mine
 		for b in self.minefield.blocks:
@@ -60,6 +63,8 @@ class Game:
 		textpos.centerx = globals.background.get_rect().centerx
 		textpos.centery = self.minefield.game_area.get_rect().top+40
 		globals.background.blit(text, textpos)
+		# wylaczenie mozliwosci odkrywania pol
+		self.clickable = False
 	def new_game(self):
 		self.minefield.draw()
 		self.minefield.set_mines()
