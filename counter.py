f1b39ca75687e80b76cdedeb76aea14332180fd6
Linoor <orange7up@gmail.com>
2014-01-12 15:31:05 +0100

    shadow counter

--- counter.py
+++ counter.py
@@ -12,10 +12,22 @@ class Counter:
 		ikona_bomby = pygame.image.load('ikonki/bomba.png')
 		counter_font = pygame.font.Font(globals.counter_and_clock_font, 27)
 		text = counter_font.render(str(self.mines).zfill(2), 1, colors.napisyColor)
+
+		# shadow
+		counter_shadow = counter_font.render(str(self.mines).zfill(2), 1, colors.shadowColor)
+		shadow_pos = pygame.Rect(self.pos)
+		shadow_pos.x += 2
+		shadow_pos.y += 2
+
+		globals.background.blit(counter_shadow, shadow_pos)
 		globals.background.blit(text, self.pos)
 		pos2 = pygame.Rect(self.pos)
 		pos2.right -= 25
 		pos2.y -= 1
+
+		
+
+
 		globals.background.blit(ikona_bomby, pos2)
 
 	def update(self):
