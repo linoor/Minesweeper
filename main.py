a096aa3e5585800b0efa17f0724deabb88538159
= <michal.pomaranski@gmail.com>
2015-10-12 11:24:01 +0200

    change from exception to print when the user has no x window system running

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ def main():
     """ główna funkcja programu """
 
     if not X_is_running():
-        raise Exception("By móc uruchomić tę grę potrzebujesz środowiska graficznego (X Window System).")
+        print("By móc uruchomić tę grę potrzebujesz środowiska graficznego (X Window System).")
         exit()
 
     pygame.font.init()
